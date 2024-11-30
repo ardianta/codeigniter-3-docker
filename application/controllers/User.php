cd75d65f73d925d184576efc5f601f81104ba63f
dian <dian@petanikode.com>
2024-11-30 23:17:08 +0800

    remove model load from controller
    it was autoloaded in config

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -5,7 +5,6 @@ class User extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-        $this->load->model('User_model');
     }
 
     public function index() {
